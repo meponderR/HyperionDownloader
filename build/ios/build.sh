f3f784639aefc2cbf6ad2244fa33f9c33f5fc2b0
meponderR <33591888+meponderR@users.noreply.github.com>
2026-06-13 23:01:15 -0500

    Bump to 0.1.1

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="hyperion-downloader.exe"
 BUNDLE_ID="com.example.hyperiondownloader"
-VERSION="0.1.0"
-BUILD_NUMBER="0.1.0"
+VERSION="0.1.1"
+BUILD_NUMBER="0.1.1"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
